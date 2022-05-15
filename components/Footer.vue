994ade8823393a2faac2f02a043753a4cd6510fa
M Atif <chatif476@gmail.com>
2022-05-15 12:31:56 +0500

    Footer set

--- components/Footer.vue
+++ components/Footer.vue
@@ -1,9 +1,9 @@
 <template>
   <section class="glass w-full border-b border-[#00dc801e]">
     <p class="py-4 flex-cen text-white-primary text-xs">
-      Code with depression by 
-      <span class=" text-green border-b mx-1"> 
-        <a href="https://github.com/atif0075"> Atif</a>
+      Code with depression by
+      <span class="text-green border-b mx-1">
+        <a target="_blank" href="https://github.com/atif0075"> Atif</a>
       </span>
     </p>
   </section>
